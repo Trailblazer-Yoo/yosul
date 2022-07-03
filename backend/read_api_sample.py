818b7e7c977cf7091b054e188d27958e69b63b42
jjh0987 <djflswlstlf@naver.com>
2022-07-03 17:52:21 +0900

    2022-07-03_전준호

--- backend/read_api_sample.py
+++ backend/read_api_sample.py
@@ -1,9 +1,51 @@
 import requests
+import pandas as pd
+from pymongo import MongoClient
 
-# 전통주 체험프로그램 api
-url = 'https://api.odcloud.kr/api/15089109/v1/uddi:c7468573-84ff-4a92-a84b-884439ce23d3'
-key = 'sRrCYNnRHgfaquuCbMenLKhpMPLymkyrDW5T3LesX8uQ00oxDrhvmfMcI/TOVGqrnOvriGM1aKLAFIB6tQPf3w==' # decoding
-params ={'serviceKey' : key, 'pageNo' : '1', 'numOfRows' : '100', 'type' : 'json'}
+keys = ['S256yGjpf7ug3eDtNRoJBzPLPIED9Mzp3XfMp8aaoRB/FGnDuzloLHkpvLvTyddzf00SKndA/1naWcmH2ao5jg==',
+        'sRrCYNnRHgfaquuCbMenLKhpMPLymkyrDW5T3LesX8uQ00oxDrhvmfMcI/TOVGqrnOvriGM1aKLAFIB6tQPf3w==',
+        'KYnzJga7PnoRyXNVENDQkD5Ud7cbI/o/a7h/EbKR2AjRnGNYysJgMasRSJ+nis+96bWZ1YOigVVGXUzxzKQn0w==',
+        'S256yGjpf7ug3eDtNRoJBzPLPIED9Mzp3XfMp8aaoRB/FGnDuzloLHkpvLvTyddzf00SKndA/1naWcmH2ao5jg==',
+        'S256yGjpf7ug3eDtNRoJBzPLPIED9Mzp3XfMp8aaoRB/FGnDuzloLHkpvLvTyddzf00SKndA/1naWcmH2ao5jg==',
+        'sRrCYNnRHgfaquuCbMenLKhpMPLymkyrDW5T3LesX8uQ00oxDrhvmfMcI/TOVGqrnOvriGM1aKLAFIB6tQPf3w==',
+        'S256yGjpf7ug3eDtNRoJBzPLPIED9Mzp3XfMp8aaoRB/FGnDuzloLHkpvLvTyddzf00SKndA/1naWcmH2ao5jg==']
+
+urls = ['https://api.odcloud.kr/api/15042292/v1/uddi:b4eddf41-0340-421f-9c22-8a70a9defee3',
+        'https://api.odcloud.kr/api/15089109/v1/uddi:c7468573-84ff-4a92-a84b-884439ce23d3',
+        'http://api.data.go.kr/openapi/tn_pubr_public_cltur_fstvl_api']
+
+
+collection_names = ['brewery_info','brewery_programs','festival_info','loc_info_kr','loc_info_en',
+                    'sul_info01','sul_info02']
+
+# 찾아가는 양조장 샘플
+# url = 'https://api.odcloud.kr/api/15089109/v1/uddi:c7468573-84ff-4a92-a84b-884439ce23d3'
+# key = 'S256yGjpf7ug3eDtNRoJBzPLPIED9Mzp3XfMp8aaoRB/FGnDuzloLHkpvLvTyddzf00SKndA/1naWcmH2ao5jg==' # decoding
+
+# sample 3rd api
+url = 'http://api.data.go.kr/openapi/tn_pubr_public_cltur_fstvl_api'
+key = 'KYnzJga7PnoRyXNVENDQkD5Ud7cbI/o/a7h/EbKR2AjRnGNYysJgMasRSJ+nis+96bWZ1YOigVVGXUzxzKQn0w=='
+params = {'serviceKey' : key, 'pageNo' : '1', 'numOfRows' : '1000', 'type' : 'json'}
 response = requests.get(url,params=params)
+response.json()['response']['body']['items']
 
-response.json()
+
+# 안될듯하다.. -> api 마다 호출 방식이 다름
+response_bundle = []
+for i in range(len(keys)):
+    params = {'serviceKey' : keys[i], 'pageNo' : '1', 'numOfRows' : '100', 'perPage' : '100', 'type' : 'json'}
+    response = requests.get(urls[i],params=params)
+    response_bundle.append(response.json())
+
+# response.json().keys()
+# pd.DataFrame(response.json()['data'])
+# write
+
+my_client = MongoClient('mongodb://localhost:27017')
+mydb = my_client['yosul_lamp']
+mydb['programs'].insert_many(pd.DataFrame(response.json()['data']).to_dict('records'))
+
+# read
+my_client = MongoClient('mongodb://localhost:27017')
+mydb = my_client['yosul_lamp']
+pd.DataFrame(mydb['programs'].find()).drop('_id',axis=1)
