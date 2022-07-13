4aa2ac8a7e1873dc6657f2472b9ebf6ec890a3ba
Trailblazer-Yoo <yousun9591@naver.com>
2022-07-13 21:33:10 +0900

    pre

--- frontend/minutes/preprocess.py
+++ frontend/minutes/preprocess.py
@@ -7,7 +7,8 @@ with open(f'{file}.txt', 'r') as f:
 content = content.replace('이개','').replace('사실','').replace('응','').replace('만약에','').replace('뭔가',''
                 ).replace('조금','').replace('근데','').replace('약간','').replace('솔직히','').replace('또',''
                 ).replace('그러니까','').replace('이제','').replace('저희가','').replace('이거','').replace('너무','')
-content = content.replace('\n', '\n___\n### ')
+content = content.replace('\n', '\n### ')
 content = content.replace(' 참석자 1', '## 선종').replace(' 참석자 2', '## 주형').replace(' 참석자 3', '## 지우').replace(' 참석자 4', '## 지우')
+content = content.replace('#####', '___\n#####')
 with open(f'{file}_pre.md', 'w') as f:
     f.write(content)
