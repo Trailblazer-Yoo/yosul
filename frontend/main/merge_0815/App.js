8562bc1f861c50714f5e3105c44f69e5e01c3fa4
wldntls <sddyy1004@naver.com>
2022-08-16 23:04:47 +0900

    splach_sample

--- frontend/main/merge_0815/App.js
+++ frontend/main/merge_0815/App.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { SignedInStack } from "./navigation";
+import PreferredDrinkScreen from "./Screens/PreferredDrinkScreen";
 
 export default function App() {
   return (
