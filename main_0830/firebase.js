295e54da9c5814e4a8f26dfb1b39184e8e0308f8
Trailblazer-Yoo <yousun9591@naver.com>
2022-08-30 22:11:28 +0900

    seonjong

--- main_0830/firebase.js
+++ main_0830/firebase.js
@@ -4,13 +4,13 @@ import 'firebase/compat/firestore'
 import 'firebase/compat/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyByUy8ccHkkFTg3xVOEeA_hFh1zyEW68c4",
-  authDomain: "instagram-clone-coding-4a8fc.firebaseapp.com",
-  projectId: "instagram-clone-coding-4a8fc",
-  storageBucket: "instagram-clone-coding-4a8fc.appspot.com",
-  messagingSenderId: "238111638145",
-  appId: "1:238111638145:web:e0080674c37cbb0cfd8380"
-  };
+  apiKey: "AIzaSyA4i8mfNp2O_AXOfEzH3IqziXVJqD3stYs",
+  authDomain: "ios-teter.firebaseapp.com",
+  projectId: "ios-teter",
+  storageBucket: "ios-teter.appspot.com",
+  messagingSenderId: "1086202831260",
+  appId: "1:1086202831260:web:89b3a4bc6a97dfc030e859"
+};
 
 !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app()
 
