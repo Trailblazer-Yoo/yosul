deab12f1ab2e39469ab8016185f16bb6fc1c6250
Trailblazer-Yoo <yousun9591@naver.com>
2022-09-08 09:13:26 +0900

    0908

--- main_0830/Screens/DictionaryScreen.js
+++ main_0830/Screens/DictionaryScreen.js
@@ -10,35 +10,56 @@ import {
   Pressable,
 } from "react-native";
 import React, { useState, useEffect } from "react";
-import { Octicons } from "@expo/vector-icons";
 import firebase from "../firebase";
+import BookmarkIcon from "../Components/Dictionary/BookmarkIcon";
+import HeartIcon from "../Components/Dictionary/HeartIcon";
+
+const db = firebase.firestore()
 
 export default function DictionaryScreen({ navigation }) {
   const [loading, setLoading] = useState(false);
-  const [sulList, setSulList] = useState([]);
-  const sulCollection = firebase.firestore().collection("global");
-  // const sulCollection = firebase.firestore().collection("soo_test");
+  // const [sulList, setSulList] = useState([]);
+  // const sulCollection = firebase.firestore().collection("global");
+  // const getFsvDocs = async () => {
+  //   const dataSnapShot = (await sulCollection.doc("drinks").get()).data();
+  //   const data = [];
+  //   data.push(Object.values(dataSnapShot));
+  //   setSulList(data[0]);
+  //   setLoading(true);
+  //   console.log(data);
+  // };
+
+  const [sulList, setSulList] = useState({});
+
+  const sulCollection = firebase.firestore().collection("sool_test");
+
   const getFsvDocs = async () => {
-    const dataSnapShot = (await sulCollection.doc("drinks").get()).data();
-    // const dataSnapShot = (await sulCollection.doc("init").get()).data();
-    // const dataCols = Object.keys(dataSnapShot);
-    // const sulName = dataSnapShot["name"];
-    // const data = [];
-    // for (let i = 0; i < sulName.length; i++) {
-    //   let temp = {};
-    //   dataCols.map((x) => (temp[x] = dataSnapShot[x][i]));
-    //   data.push(temp);
-    // }
+    const dataSnapShot = (await sulCollection.doc("init").get()).data();
+    const dataCols = Object.keys(dataSnapShot);
+    const sulName = dataSnapShot["name"];
     const data = [];
-    data.push(Object.values(dataSnapShot));
-    setSulList(data[0]);
+    for (let i = 0; i < sulName.length; i++) {
+      let temp = {};
+      dataCols.map((x) => (temp[x] = dataSnapShot[x][i]));
+      data.push(temp);
+    }
+    setSulList(data);
     setLoading(true);
-    console.log(data);
+    // console.log(dataSnapShot);
   };
 
   useEffect(() => {
     getFsvDocs();
   }, []);
+
+  // const handlelike = async () => {
+  //   const currentUserEmail = firebase.auth().currentUser.email;
+
+  //   const a = (await db.collection('users').doc(currentUserEmail).collection('likse_alcohol').get()).data()
+  //   const currentLikesStatus = !post.likes_by_users.includes();
+
+    
+  // };
 
   const renderListItem = ({ item, index }) => {
     return (
@@ -52,29 +73,20 @@ export default function DictionaryScreen({ navigation }) {
           <Image style={styles.img} source={{ uri: item.img }} />
           <View style={styles.textcontainer}>
             <Text style={styles.name}>{item.name}</Text>
-            {/* <Text style={styles.text}>
+            <Text style={styles.text}>
               지역 :{" "}
               {item.address.substr(
                 0,
                 item.address.indexOf(" ", item.address.indexOf(" ") + 1)
               )}
-            </Text> */}
-            <Text style={styles.text}>도수 :{item.alcohol}</Text>
-            <Text style={styles.text}>용량 : {item.capacity}</Text>
-            <Text style={styles.text}>주재료 : {item.materials}</Text>
-            <Text style={styles.text}>제조 : {item.craft}</Text>
-            {/* <Text style={styles.text}>분류 : {item.category}</Text> */}
+            </Text>
+            <Text style={styles.text}>도수 :{item.alc}</Text>
+            <Text style={styles.text}>분류 : {item.category}</Text>
           </View>
         </Pressable>
         <View style={styles.iconcontainer}>
-          <TouchableOpacity style={{marginBottom:2}}>
-            {/* heart / heart-fill */}
-            <Octicons name="heart" size={27} color="gray" />
-          </TouchableOpacity>
-          <TouchableOpacity>
-            {/* star / start-fill */}
-          <Octicons name="star" size={27} color="gray" />
-          </TouchableOpacity>
+          <HeartIcon />
+          <BookmarkIcon />
         </View>
       </View>
     );
@@ -120,8 +132,7 @@ const styles = StyleSheet.create({
     justifyContent: "space-between",
     flexDirection: "row",
     width: Dimensions.get("window").width - 20,
-    height: (Dimensions.get("window").width) / 2.5,
-    // height: (Dimensions.get("window").width) - 20 / 4,
+    height: (Dimensions.get("window").width - 20) / 4,
     padding: 8,
   },
   item: {
@@ -158,7 +169,7 @@ const styles = StyleSheet.create({
     marginBottom: 1,
   },
   iconcontainer: {
-    marginTop:4,
-    alignItems:'center',
+    marginTop: 4,
+    alignItems: "center",
   },
 });
