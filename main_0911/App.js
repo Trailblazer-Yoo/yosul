14227035f0e14f6b28905512f0e63c2d83fccd5b
parkjubro <juhyeong97@gmail.com>
2022-09-14 23:58:01 +0900

    pjh_statusBar

--- main_0911/App.js
+++ main_0911/App.js
@@ -1,10 +1,15 @@
 import * as React from "react";
+import { View } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import { SignedInStack, SignedOutStack } from "./navigation";
 import AuthNavigation from "./AuthNavigation";
 
 export default function App() {
   return (
-      // <SignedOutStack />
+    // <SignedOutStack />
+    <>
       <AuthNavigation />
+      <StatusBar style="auto" />
+    </>
   );
 }
